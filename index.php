85d3fd1910424e1e1a9e4fd8c32c0bd22b469539
koodohyun <95936722+koodohyun@users.noreply.github.com>
2021-12-11 04:51:42 +0900

    ver2

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="UTF-8">
+
+ </head>
+  <body>
+
+  <?php
+  echo "ffff";
+  echo "string";
+  echo 1+1;
+  echo "1"+"2";
+   ?>
+
+
+
+  </body>
+</html>
